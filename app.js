c42524e160813f011b968973ed83bb6a5d3bde93
uzairshekhani <uzairshekhani2017@gmail.com>
2023-11-02 04:05:22 -0700

    Portfolio

---
+++ app.js
@@ -0,0 +1,21 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+  sidemenu.style.right = "0"
+}
+function closemenu(){
+  sidemenu.style.right = "-200px"
+}
